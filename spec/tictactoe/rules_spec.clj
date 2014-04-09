7e6f634408ec343e0bb31c1bb4fd23942a102051
Mike Knepper <michaelrknepper@gmail.com>
2014-04-09 16:55:25 -0500

    Adds rule to check if all spots on the board are taken

--- spec/tictactoe/rules_spec.clj
+++ spec/tictactoe/rules_spec.clj
@@ -5,4 +5,12 @@
 (describe "rules"
   (it "knows a spot is not valid if it has been played"
     (let [spots [:x nil nil nil]]
-      (should= false (valid-spot? spots 0)))))
+      (should= false (valid-spot? spots 0))))
+
+  (it "recognizes all spots as being taken"
+    (let [spots [:x :o :x :o]]
+      (should= true (all-spots-taken? spots))))
+
+  (it "recognizes spots still open to be played"
+    (let [spots [:x :o :z nil]]
+      (should= false (all-spots-taken? spots)))))
--- src/tictactoe/rules.clj
+++ src/tictactoe/rules.clj
@@ -2,3 +2,6 @@
 
 (defn valid-spot? [spots index]
   (= nil (spots index)))
+
+(defn all-spots-taken? [spots]
+  (= 0 (count (filter (fn [spot] (= spot nil)) spots))))
