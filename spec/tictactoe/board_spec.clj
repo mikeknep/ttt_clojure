7178396e3dd6c47eb6fc6b25bc67fc657a71d325
Mike Knepper <michaelrknepper@gmail.com>
2014-04-09 14:59:10 -0500

    Passes size argument to board spaces to determine board size

--- spec/tictactoe/board_spec.clj
+++ spec/tictactoe/board_spec.clj
@@ -3,14 +3,17 @@
             [tictactoe.board :refer :all]))
 
 (describe "a board"
-  (it "has nine spaces"
-    (should= 9 (count (spaces{}))))
+  (it "has total number of spaces equal to the square of the size"
+    (should= 9 (count (spaces 3 {}))))
+
+  (it "has 16 spaces if the size provided is 4"
+    (should= 16 (count (spaces 4 {}))))
 
   (it "can have an x in the first spot"
-    (should= :x (first (spaces{0 :x}))))
+    (should= :x (first (spaces 3 {0 :x}))))
 
   (it "can have an o in the last spot"
-    (should= :o (last (spaces{8 :o}))))
+    (should= :o (last (spaces 3 {8 :o}))))
 
   (it "can have tokens in various spots"
-    (should= [:x nil :o nil :o :x nil nil nil] (spaces{0 :x 2 :o 4 :o 5 :x}))))
+    (should= [:x nil :o nil :o :x nil nil nil] (spaces 3 {0 :x 2 :o 4 :o 5 :x}))))
--- src/tictactoe/board.clj
+++ src/tictactoe/board.clj
@@ -1,10 +1,10 @@
 (ns tictactoe.board)
 
-(defn spaces [tokens]
+(defn spaces [size tokens]
   (loop [tokens   tokens
          spots    []
          counter  0]
-    (if (= 9 counter)
+    (if (= (* size size) counter)
       spots
       (if (= counter (first (sort (keys tokens))))
         (recur (dissoc tokens counter) (conj spots (tokens counter)) (inc counter))
