7663174e56476a7ae288979d4f2139941385ebe5
Mike Knepper <michaelrknepper@gmail.com>
2014-04-10 15:58:18 -0500

    Rules checks whole board for winner; contexts in rules spec for readability

--- spec/tictactoe/rules_spec.clj
+++ spec/tictactoe/rules_spec.clj
@@ -7,22 +7,32 @@
     (let [spots [:x nil nil nil]]
       (should= false (valid-spot? spots 0))))
 
-  (it "recognizes all spots as being taken"
-    (let [spots [:x :o :x :o]]
-      (should= true (all-spots-taken? spots))))
+  (context "checking for a draw"
+    (it "recognizes all spots as being taken"
+      (let [spots [:x :o :x :o]]
+        (should= true (all-spots-taken? spots))))
 
-  (it "recognizes spots still open to be played"
-    (let [spots [:x :o :z nil]]
-      (should= false (all-spots-taken? spots))))
+    (it "recognizes spots still open to be played"
+      (let [spots [:x :o :z nil]]
+        (should= false (all-spots-taken? spots)))))
 
-  (it "returns the token from a winning collection"
-    (let [tokens  [:x :x :x]]
-      (should= :x (winner-in-collection? tokens))))
+  (context "checking a single path for a winner"
+    (it "returns the token from a winning collection"
+      (let [tokens  [:x :x :x]]
+        (should= :x (winner-in-collection? tokens))))
 
-  (it "returns nil if a collection has multiple tokens"
-    (let [tokens [:x :o :x]]
-      (should= nil (winner-in-collection? tokens))))
+    (it "returns nil if a collection has multiple tokens"
+      (let [tokens [:x :o :x]]
+        (should= nil (winner-in-collection? tokens))))
 
-  (it "returns nil if a collection has all empty spaces"
-    (let [tokens [nil nil nil]]
-      (should= nil (winner-in-collection? tokens)))))
+    (it "returns nil if a collection has all empty spaces"
+      (let [tokens [nil nil nil]]
+        (should= nil (winner-in-collection? tokens)))))
+
+  (it "returns the winning token from a board with a winner"
+    (let [board {:size 3 :spots [:x nil nil :x nil nil :x nil nil]}]
+      (should= :x (winner-on-board? board))))
+
+  (it "returns nil for a board with no winner"
+    (let [board {:size 3 :spots (repeat 9 nil)}]
+      (should= nil (winner-on-board? board)))))
--- src/tictactoe/rules.clj
+++ src/tictactoe/rules.clj
@@ -1,4 +1,6 @@
-(ns tictactoe.rules)
+(ns tictactoe.rules
+  (:require [tictactoe.board-shaper :refer [all-winning-indexes]]
+            [tictactoe.board :refer [values-at-indexes]]))
 
 (defn valid-spot? [spots index]
   (= nil (spots index)))
@@ -9,3 +11,12 @@
 (defn winner-in-collection? [tokens]
   (if (and (= 1 (count (distinct tokens))) (complement (nil? (first (distinct tokens)))))
     (first (distinct tokens))))
+
+(defn winner-on-board? [board]
+  (loop [paths  (all-winning-indexes (get board :size))
+         spots  (get board :spots)]
+    (if (winner-in-collection? (values-at-indexes (first paths) spots))
+      (winner-in-collection? (values-at-indexes (first paths) spots))
+      (if (= 0 (count paths))
+        nil
+        (recur (rest paths) spots)))))
