4cfece3a32888340710ad17d7a44851d935b1530
Mike Knepper <michaelrknepper@gmail.com>
2014-04-10 13:39:19 -0500

    Simple ai choose spot only needs spots, not a full board

--- spec/tictactoe/simple_ai_spec.clj
+++ spec/tictactoe/simple_ai_spec.clj
@@ -4,9 +4,9 @@
 
 (describe "simple ai"
   (it "selects the first spot on an empty board"
-    (let [board {:size 3 :spots (repeat 9 nil)}]
-      (should= 0 (choose-spot board))))
+    (let [spots (repeat 9 nil)]
+      (should= 0 (choose-spot spots))))
   
   (it "selects the first available spot"
-    (let [board {:size 3 :spots [:x :x :o nil nil nil nil nil nil]}]
-      (should= 3 (choose-spot board)))))
+    (let [spots [:x :x :o nil nil nil nil nil nil]]
+      (should= 3 (choose-spot spots)))))
--- src/tictactoe/simple_ai.clj
+++ src/tictactoe/simple_ai.clj
@@ -1,4 +1,4 @@
 (ns tictactoe.simple_ai)
 
-(defn choose-spot [board]
-  (first (keep-indexed #(if (= nil %2) %1) (board :spots))))
+(defn choose-spot [spots]
+  (first (keep-indexed #(if (= nil %2) %1) spots)))
