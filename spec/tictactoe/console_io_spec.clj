10be1be662e900a3caad44ca00c520c0a05c6a29
Mike Knepper <michaelrknepper@gmail.com>
2014-04-14 10:12:56 -0500

    Adds functions for displaying a board

--- spec/tictactoe/console_io_spec.clj
+++ spec/tictactoe/console_io_spec.clj
@@ -12,4 +12,33 @@
   (it "asks for a board size and returns an integer"
     (should= 3
       (with-in-str "3"
-        (get-board-size)))))
+        (get-board-size))))
+
+  (context "visually representing a board"
+    (it "represents a spot played by :x as X"
+      (should= "X" (display-spot :x)))
+
+    (it "represents a spot played by :o as O"
+      (should= "O" (display-spot :o)))
+
+    (it "represents an unplayed spot as a blank space"
+      (should= " " (display-spot nil)))
+
+    (it "displays a pipe to separate spaces in a row"
+      (should= "|" (pipe-or-newline 0 3)))
+
+    (it "returns a new line to separate the space at the end of a row from the next row"
+      (should= "\n" (pipe-or-newline 2 3)))
+
+    (it "converts a new/empty board into a string representation of an empty board"
+      (let [board   {:size 3 :spots (repeat 9 nil)}]
+        (should= " | | \n | | \n | | \n" (format-board board))))
+
+    (it "converts a board with spots that have been played into a string representation of a board with moves"
+      (let [board   {:size 3 :spots [nil :x :o nil :x :o nil :o :x]}]
+        (should= " |X|O\n |X|O\n |O|X\n" (format-board board))))
+
+    (it "prints a formatted board"
+      (let [board   {:size 3 :spots (repeat 9 nil)}]
+        (should= (str (format-board board) "\n")
+          (with-out-str (display-board board)))))))
--- src/tictactoe/console_io.clj
+++ src/tictactoe/console_io.clj
@@ -7,3 +7,26 @@
 (defn get-board-size []
   (println "What size board do you want to play on?")
   (read-string (read-line)))
+
+(defn display-spot [spot]
+  (case spot
+        :x    "X"
+        :o    "O"
+        nil   " "))
+
+(defn pipe-or-newline [spot-index board-size]
+  (if (= 0 (mod (+ 1 spot-index) board-size))
+    "\n"
+    "|"))
+
+(defn format-board [board]
+  (loop [spots          (get board :spots)
+         size           (get board :size)
+         board-string   ""
+         spot-index     0]
+    (if (empty? spots)
+      board-string
+      (recur (rest spots) size (str board-string (display-spot (first spots)) (pipe-or-newline spot-index size)) (inc spot-index)))))
+
+(defn display-board [board]
+  (println (format-board board)))
