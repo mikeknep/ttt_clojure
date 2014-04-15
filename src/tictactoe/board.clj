e1faa4571668ff15f802fe11224e7fdc1492ddaf
Mike Knepper <michaelrknepper@gmail.com>
2014-04-15 10:46:52 -0500

    Refactors board functions

--- src/tictactoe/board.clj
+++ src/tictactoe/board.clj
@@ -5,7 +5,7 @@
          counter  0]
     (if (= counter (* size size))
       {:size size :spots spots}
-      (recur (assoc spots counter nil) (inc counter)))))
+      (recur (conj spots nil) (inc counter)))))
 
 (defn update-board [size spots]
   {:size    size
@@ -13,8 +13,7 @@
 
 (defn values-at-indexes [indexes spots]
   (loop [indexes  indexes
-         spots    spots
          tokens   []]
     (if (empty? indexes)
       tokens
-      (recur (rest indexes) spots (conj tokens (nth spots (first indexes)))))))
+      (recur (rest indexes) (conj tokens (nth spots (first indexes)))))))
