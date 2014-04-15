35873d5054ea2ab09bfdf8048d4ee688f3603c49
Mike Knepper <michaelrknepper@gmail.com>
2014-04-15 10:23:33 -0500

    Adds whose turn declaration to game

--- src/tictactoe/game_runner.clj
+++ src/tictactoe/game_runner.clj
@@ -1,6 +1,6 @@
 (ns tictactoe.game-runner
   (:require [tictactoe.rules :refer [game-over? current-token]]
-            [tictactoe.console-io :refer [display-board declare-result]]
+            [tictactoe.console-io :refer [display-board display-spot declare-whose-turn declare-result]]
             [tictactoe.game :refer [get-player create-game]]
             [tictactoe.board :refer [update-board]]
             [tictactoe.gameplay :refer [take-turn]]))
@@ -14,7 +14,8 @@
       (display-board board)
       (if (game-over? board)
         (declare-result board)
-        (recur (update-board (get board :size) (take-turn spots (next-turn-fn spots) cur-token)))))))
+        (do (declare-whose-turn (display-spot cur-token))
+        (recur (update-board (get board :size) (take-turn spots (next-turn-fn spots) cur-token))))))))
 
 (defn -main []
   (play-game (create-game)))
