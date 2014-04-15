2f804dc78cb758d396873ca9db94b4b5e51816e7
Mike Knepper <michaelrknepper@gmail.com>
2014-04-15 09:38:56 -0500

    Creates board with loop/recur instead of lazy sequence

--- src/tictactoe/board.clj
+++ src/tictactoe/board.clj
@@ -1,8 +1,11 @@
 (ns tictactoe.board)
 
 (defn create-board [size]
-  {:size    size
-   :spots   (repeat (* size size) nil)})
+  (loop [spots    []
+         counter  0]
+    (if (= counter (* size size))
+      {:size size :spots spots}
+      (recur (assoc spots counter nil) (inc counter)))))
 
 (defn update-board [size spots]
   {:size    size
