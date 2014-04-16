1a994fde2fffb94cea04513effb7be0437e8aedd
Mike Knepper <michaelrknepper@gmail.com>
2014-04-16 11:13:02 -0500

    Refactors board scoring methods; using 'with' in spec

--- spec/tictactoe/unbeatable_ai_spec.clj
+++ spec/tictactoe/unbeatable_ai_spec.clj
@@ -3,19 +3,25 @@
             [tictactoe.unbeatable-ai :refer :all]))
 
 (describe "unbeatable ai"
-  (context "scoring a board"
-    (it "scores a board with a cat's game zero 0"
-      (let [board {:size 3 :spots [:x :o :x :x :o :x :o :x :o]}]
-        (should= 0 (score-board board 0))))
+  (with draw-board      {:size 3 :spots [:x :o :x :x :o :x :o :x :o]})
+  (with basic-win-board {:size 3 :spots [:x :x :x nil nil nil nil nil nil]})
 
-    (it "scores a board with a winner at zero depth level +1.0"
-      (let [board {:size 3 :spots [:x :x :x nil nil nil nil nil nil]}]
-        (should= 1.0 (score-board board 0))))
+  (context "basic scoring of a board"
+    (it "scores a board with a cat's game 0.0"
+      (should= 0.0 (simple-board-score @draw-board)))
 
-    (it "scores a board with a winner at one depth level -1.0"
-      (let [board {:size 3 :spots [:x :x :x nil nil nil nil nil nil]}]
-        (should= -1.0 (score-board board 1))))
+    (it "scores a board with a winner 1.0"
+      (should= 1.0 (simple-board-score @basic-win-board))))
 
-    (it "scores a board with a winner at two depth levels +1.0"
-      (let [board {:size 3 :spots [:x :x :x nil nil nil nil nil nil]}]
-        (should= 1.0 (score-board board 2))))))
+  (context "scoring a board with depth"
+    (it "scores a winning board at zero depth level +Infinity"
+      (should= (/ 1.0 0) (score-board-with-depth @basic-win-board 0)))
+
+    (it "scores a winning board at one depth level -1.0"
+      (should= -1.0 (score-board-with-depth @basic-win-board 1)))
+
+    (it "scores a winning board at two depth levels +0.5"
+      (should= 0.5 (score-board-with-depth @basic-win-board 2)))
+
+    (it "scores a draw board at any depth 0"
+      (should= 0 (score-board-with-depth @draw-board 0)))))
--- src/tictactoe/unbeatable_ai.clj
+++ src/tictactoe/unbeatable_ai.clj
@@ -1,7 +1,12 @@
 (ns tictactoe.unbeatable-ai
   (:require [tictactoe.rules :refer [all-spots-taken? winner-on-board?]]))
 
-(defn score-board [board depth]
+(defn simple-board-score [board]
   (if (winner-on-board? board)
-    (double (reduce * (repeat depth -1.0)))
-    0))
+    1.0
+    0.0))
+
+(defn score-board-with-depth [board depth]
+  (if (zero? (simple-board-score board))
+    0
+    (* (/ (simple-board-score board) (double depth)) (reduce * (repeat depth -1)))))
