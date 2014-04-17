e9f97279ac90ca18d8033434a8266c4a0268295a
Mike Knepper <michaelrknepper@gmail.com>
2014-04-17 11:02:00 -0500

    Adds unbeatable ai minimax scoring and choose best spot functions

--- src/tictactoe/unbeatable_ai.clj
+++ src/tictactoe/unbeatable_ai.clj
@@ -1,5 +1,7 @@
 (ns tictactoe.unbeatable-ai
-  (:require [tictactoe.rules :refer [all-spots-taken? winner-on-board?]]))
+  (:require [tictactoe.rules :refer [available-spots game-over? all-spots-taken? winner-on-board?]]
+            [tictactoe.board :refer [update-board]]
+            [tictactoe.gameplay :refer [take-turn]]))
 
 (defn simple-board-score [board]
   (if (winner-on-board? board)
@@ -20,3 +22,28 @@
   (if (> this-score best-score)
     this-spot
     best-spot))
+
+(defn create-altered-board [board index token]
+  (update-board (get board :size) (take-turn (get board :spots) index token)))
+
+
+
+(defn minimax [board current-token opponent-token depth]
+  (if (game-over? board)
+    (score-board-with-depth board depth)
+    (apply min (map minimax (map create-altered-board (repeat board) (available-spots (get board :spots)) (repeat opponent-token)) (repeat opponent-token) (repeat current-token) (repeat (+ 1 depth))))))
+
+
+(defn choose-best-spot [board current-token opponent-token]
+  (loop [best-score   (/ -1.0 0)
+         best-spot    -1
+         open-spots   (available-spots (get board :spots))]
+    (if (empty? open-spots)
+      best-spot
+      (let [this-spot       (first open-spots)
+            altered-board   (create-altered-board board this-spot current-token)
+            this-score      (minimax altered-board current-token opponent-token 0)]
+        (recur
+          (update-best-score this-score best-score)
+          (update-best-spot this-spot this-score best-spot best-score)
+          (rest open-spots))))))
