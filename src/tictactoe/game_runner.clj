92228a181031c395d82333cfa16d48fc3c881db5
Mike Knepper <michaelrknepper@gmail.com>
2014-04-17 11:00:31 -0500

    Game runner passes args needed for unbeatable ai, plus improved readability

--- src/tictactoe/game_runner.clj
+++ src/tictactoe/game_runner.clj
@@ -1,5 +1,5 @@
 (ns tictactoe.game-runner
-  (:require [tictactoe.rules :refer [game-over? current-token]]
+  (:require [tictactoe.rules :refer [game-over? current-token opponent-token]]
             [tictactoe.console-io :refer [display-board display-spot declare-whose-turn declare-result]]
             [tictactoe.game :refer [get-player create-game]]
             [tictactoe.board :refer [update-board]]
@@ -8,14 +8,15 @@
 (defn play-game [game]
   (loop [board            (get game :board)]
     (let [spots           (get board :spots)
-          cur-token       (current-token spots)
-          cur-player      (get-player game cur-token)
-          next-turn-fn    (get cur-player :decision-maker)]
+          current-token   (current-token spots)
+          opponent-token  (opponent-token spots)
+          current-player  (get-player game current-token)
+          next-turn-fn    (get current-player :decision-maker)]
       (display-board board)
       (if (game-over? board)
         (declare-result board)
-        (do (declare-whose-turn (display-spot cur-token))
-        (recur (update-board (get board :size) (take-turn spots (next-turn-fn spots) cur-token))))))))
+        (do (declare-whose-turn (display-spot current-token))
+        (recur (update-board (get board :size) (take-turn spots (next-turn-fn board current-token opponent-token) current-token))))))))
 
 (defn -main []
   (play-game (create-game)))
