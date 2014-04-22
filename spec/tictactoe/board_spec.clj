6aee880908239c7746d0f56a679c46cf804d7f82
Mike Knepper <michaelrknepper@gmail.com>
2014-04-22 14:16:37 -0500

    Clearer distinction between board size and board length

--- spec/tictactoe/board_spec.clj
+++ spec/tictactoe/board_spec.clj
@@ -4,12 +4,15 @@
 
 (describe "a board"
   (it "is created with empty spots"
-    (should= (repeat 9 nil) (create-board 3)))
+    (should= [nil] (distinct (create-board 3))))
 
   (it "has a number of spots equal to the square of the length"
     (should= 16 (count (create-board 4))))
 
+  (it "has a length equal to the square root of the number of spots"
+    (should= 3 (board-length (create-board 3))))
+
   (it "returns the tokens at provided indexes"
     (let [indexes [0 2 4]
-          spots   [:x nil :o nil nil]]
-      (should= [:x :o nil] (values-at-indexes indexes spots)))))
+          board   [:x nil :o nil nil]]
+      (should= [:x :o nil] (values-at-indexes indexes board)))))
--- src/tictactoe/board.clj
+++ src/tictactoe/board.clj
@@ -1,11 +1,14 @@
 (ns tictactoe.board)
 
-(defn create-board [size]
+(defn create-board [length]
   (loop [spots    []
          counter  0]
-    (if (= counter (* size size))
+    (if (= counter (* length length))
       spots
       (recur (conj spots nil) (inc counter)))))
+
+(defn board-length [board]
+  (int (Math/sqrt (count board))))
 
 (defn values-at-indexes [indexes board]
   (loop [indexes  indexes
