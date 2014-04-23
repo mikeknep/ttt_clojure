1ec1297f733a3718d4e9d67570caa4236e7a7ced
Mike Knepper <michaelrknepper@gmail.com>
2014-04-23 09:49:34 -0500

    Updates player spec with custom tokens

--- spec/tictactoe/player_spec.clj
+++ spec/tictactoe/player_spec.clj
@@ -6,10 +6,10 @@
 
 (describe "a player"
   (it "is created with a token"
-    (should= :x ((create-player :x choose-random-spot) :token)))
+    (should= "X" ((create-player "X" choose-random-spot) :token)))
 
   (it "is created with the simple ai's choose-random-spot function when it is an easy computer"
-    (should= choose-random-spot ((create-player :x choose-random-spot) :decision-maker)))
+    (should= choose-random-spot ((create-player "X" choose-random-spot) :decision-maker)))
 
   (it "is created with the console io's get-move function when it is a human player"
-    (should= get-move ((create-player :o get-move) :decision-maker))))
+    (should= get-move ((create-player "O" get-move) :decision-maker))))
