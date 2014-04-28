e67383157beed62fd830ab1b0532f9abd2ceeb4c
Mike Knepper <michaelrknepper@gmail.com>
2014-04-28 11:35:31 -0500

    Moves all validity-check fns inline in prompts; removes now-unneeded console validator ns

--- src/tictactoe/console_prompter.clj
+++ src/tictactoe/console_prompter.clj
@@ -1,23 +1,21 @@
-(ns tictactoe.console-prompter
-  (:require [tictactoe.console-validator :refer [valid-player-type? valid-token? valid-play-again-response?]]
-            [tictactoe.player :refer [set-player-decision-maker]]))
+(ns tictactoe.console-prompter)
 
 (def get-player-type
   {:first-prompt "What type of player is this? ('human' 'easy computer' or 'hard computer')"
    :second-prompt "That is not a valid player type. Please input 'human' 'easy computer' or 'hard computer'."
-   :validity-checker valid-player-type?
-   :followup-fn set-player-decision-maker})
+   :validity-checker #(contains? #{"human" "easy computer" "hard computer"} %)
+   :followup-fn eval})
 
 (def get-player-token
   {:first-prompt "What is this player's token?"
    :second-prompt "That is not a valid token. Please select a unique, one-character token."
-   :validity-checker valid-token?
+   :validity-checker #(= 1 (count %))
    :followup-fn eval})
 
 (def get-play-again
   {:first-prompt "Play again? (y/n)"
    :second-prompt "Do you want to play again or not? (y/n)"
-   :validity-checker valid-play-again-response?
+   :validity-checker #(contains? #{"y" "n"} %)
    :followup-fn #(= "y" %)})
 
 (defn prompt [template]
