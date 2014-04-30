108c5f8d4e5ba29262c26b71f42513c010176c70
Mike Knepper <michaelrknepper@gmail.com>
2014-04-30 10:39:09 -0500

    Moves english txt file to new translations directory

--- src/tictactoe/parser.clj
+++ src/tictactoe/parser.clj
@@ -4,28 +4,28 @@
   (nth (with-open [rdr (clojure.java.io/reader file)] (reduce conj [] (line-seq rdr))) line-number))
 
 (def current-player-text
-  (parse "english.txt" 0))
+  (parse "translations/english.txt" 0))
 
 (def draw-text
-  (parse "english.txt" 1))
+  (parse "translations/english.txt" 1))
 
 (def winner-text
-  (parse "english.txt" 2))
+  (parse "translations/english.txt" 2))
 
 (def player-type-prompt-1
-  (parse "english.txt" 3))
+  (parse "translations/english.txt" 3))
 
 (def player-type-prompt-2
-  (parse "english.txt" 4))
+  (parse "translations/english.txt" 4))
 
 (def player-token-prompt-1
-  (parse "english.txt" 5))
+  (parse "translations/english.txt" 5))
 
 (def player-token-prompt-2
-  (parse "english.txt" 6))
+  (parse "translations/english.txt" 6))
 
 (def play-again-prompt-1
-  (parse "english.txt" 7))
+  (parse "translations/english.txt" 7))
 
 (def play-again-prompt-2
-  (parse "english.txt" 8))
+  (parse "translations/english.txt" 8))
