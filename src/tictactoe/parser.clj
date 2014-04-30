fe67d749d8f482495c05380e951e85dfe87b4793
Mike Knepper <michaelrknepper@gmail.com>
2014-04-30 14:36:39 -0500

    Parser gets language source from language namespace

--- src/tictactoe/parser.clj
+++ src/tictactoe/parser.clj
@@ -1,34 +1,32 @@
-(ns tictactoe.parser)
+(ns tictactoe.parser
+  (:require [tictactoe.language :refer [language-source]]))
 
 (defn parse [file line-number]
   (nth (with-open [rdr (clojure.java.io/reader file)] (reduce conj [] (line-seq rdr))) line-number))
 
-(def language-source
-  "translations/english.txt")
-
 (def current-player-text
-  (parse language-source 0))
+  (parse (language-source) 0))
 
 (def draw-text
-  (parse language-source 1))
+  (parse (language-source) 1))
 
 (def winner-text
-  (parse language-source 2))
+  (parse (language-source) 2))
 
 (def player-type-prompt-1
-  (parse language-source 3))
+  (parse (language-source) 3))
 
 (def player-type-prompt-2
-  (parse language-source 4))
+  (parse (language-source) 4))
 
 (def player-token-prompt-1
-  (parse language-source 5))
+  (parse (language-source) 5))
 
 (def player-token-prompt-2
-  (parse language-source 6))
+  (parse (language-source) 6))
 
 (def play-again-prompt-1
-  (parse language-source 7))
+  (parse (language-source) 7))
 
 (def play-again-prompt-2
-  (parse language-source 8))
+  (parse (language-source) 8))
