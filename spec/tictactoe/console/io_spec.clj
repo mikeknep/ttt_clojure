1c3dfb44e3da21837cf02f6115a6f381eca4c07b
Mike Knepper <michaelrknepper@gmail.com>
2014-05-02 15:19:28 -0500

    Fixes indentation in consoleIO spec

--- spec/tictactoe/console/io_spec.clj
+++ spec/tictactoe/console/io_spec.clj
@@ -12,22 +12,22 @@
                        :followup-fn count})
 
   (context "prompting the user for input"
-  (it "prints the first prompt"
-    (should-contain "Hello\n"
-      (with-out-str
+    (it "prints the first prompt"
+      (should-contain "Hello\n"
+        (with-out-str
+          (with-in-str "foo"
+            (prompt @mock-template)))))
+
+    (it "performs a followup function on a valid input"
+      (should= 3
         (with-in-str "foo"
-          (prompt @mock-template)))))
+          (prompt @mock-template))))
 
-  (it "performs a followup function on a valid input"
-    (should= 3
-      (with-in-str "foo"
-        (prompt @mock-template))))
-
-  (it "prints the second prompt when the input is invalid"
-    (should-contain "World\n"
-      (with-out-str
-        (with-in-str "bar\nfoo"
-          (prompt @mock-template))))))
+    (it "prints the second prompt when the input is invalid"
+      (should-contain "World\n"
+        (with-out-str
+          (with-in-str "bar\nfoo"
+            (prompt @mock-template))))))
 
   (context "displaying text on the screen without expecting human input"
     (it "prints to the screen"
